cc45e48ce5804f358b6b023f900880f947155489
Psaltus <matt.psaltis15@gmail.com>
2020-01-22 22:50:23 -0500

    Initial commit

---
+++ Source/Udemy/MovingPlatform.h
@@ -0,0 +1,37 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/StaticMeshActor.h"
+#include "MovingPlatform.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UDEMY_API AMovingPlatform : public AStaticMeshActor
+{
+	GENERATED_BODY()
+	
+public:
+	AMovingPlatform();
+	virtual void BeginPlay() override;
+	
+	virtual void Tick(float DeltaTime) override;
+
+	//virtual FVector CalculateMovement(FVector OriginalLocation, FVector TargetLocation);
+
+	UPROPERTY(EditAnywhere)
+	float Speed = 20;
+
+	UPROPERTY(EditAnywhere, Meta = (MakeEditWidget = true))
+	FVector TargetLocation;
+
+	
+private:
+	FVector GlobalStartLocation;
+	FVector GlobalTargetLocation;
+	FVector Direction;
+	
+};
